1b4c7e209ef3928a53549350987c12f7ba6e9b89
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-08-21 15:53:14 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+import sklearn
+
+from pandas import Series, DataFrame
+from sklearn import preprocessing
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import classification_report
